5ed4e67f05776fafee646efc07dd50ee9885b1f4
katekkq <katekohkq@gmail.com>
2026-04-29 10:06:50 +0800

    [Kate] Added player sprite, background, and GameObject Triangle that prints 'I'm a triangle.'

---
+++ Assets/Scripts/Triangle.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Triangle : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("I'm a triangle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

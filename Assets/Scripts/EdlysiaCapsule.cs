b656699db4ff19cc5daaaef61674948c0e50afd2
wenomechainsama6 <edlysiag@gmail.com>
2026-05-06 09:53:46 +0800

    [Edlysia] Add GameObject Capsule, Add Script

---
+++ Assets/Scripts/EdlysiaCapsule.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.LowLevelPhysics;
+
+public class EdlysiaCapsule : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("capsule");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
